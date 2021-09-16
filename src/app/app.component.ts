89f1ae94cef781ef0f3c3dc2116e98b2accb6a13
Martin Randall <martin@coryl.com>
2021-09-16 15:31:03 +0100

    Classes working fine

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, TemplateRef, ViewContainerRef } from '@angular/core';
+import { TemplatePortal } from '@angular/cdk/portal';
+import { Overlay, OverlayRef } from '@angular/cdk/overlay';
+import { fromEvent, Subscription } from 'rxjs';
+import { take, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,69 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-context-menu-test';
+  sub: Subscription | undefined;
+
+  users = Array.from({ length: 10 }, () => ({
+    name: 'Item'
+  }));
+
+  @ViewChild('userMenu') userMenu: TemplateRef<any>;
+
+  overlayRef: OverlayRef | null;
+
+  constructor(
+    public overlay: Overlay,
+    public viewContainerRef: ViewContainerRef) {
+  }
+
+  open({ x, y }: MouseEvent, user) {
+    this.close();
+
+    const positionStrategy = this.overlay.position()
+      .flexibleConnectedTo({ x, y })
+      .withViewportMargin(8)
+      .withGrowAfterOpen(true)
+      .withPositions([
+        {
+          originX: 'end',
+          originY: 'bottom',
+          overlayX: 'end',
+          overlayY: 'top',
+        }
+      ]);
+
+    this.overlayRef = this.overlay.create({
+      positionStrategy,
+      scrollStrategy: this.overlay.scrollStrategies.close()
+    });
+
+    this.overlayRef.attach(new TemplatePortal(this.userMenu, this.viewContainerRef, {
+      $implicit: user
+    }));
+
+    this.sub = fromEvent<MouseEvent>(document, 'click')
+      .pipe(
+        filter(event => {
+          const clickTarget = event.target as HTMLElement;
+          return !!this.overlayRef && !this.overlayRef.overlayElement.contains(clickTarget);
+        }),
+        take(1)
+      ).subscribe(() => this.close())
+
+  }
+
+  delete(user) {
+    console.log('delete');
+    // delete user
+    this.close();
+  }
+
+  close() {
+    console.log('close')
+    this.sub && this.sub.unsubscribe();
+    if (this.overlayRef) {
+      this.overlayRef.dispose();
+      this.overlayRef = null;
+    }
+  }
 }
